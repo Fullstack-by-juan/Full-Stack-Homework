aa64faac7061a29d2abb51980bcd2f333161224d
camo9901 <juancamilo9901@icloud.com>
2023-04-26 22:12:14 -0400

    hoemwork

--- Block 16/index.js
+++ Block 16/index.js
@@ -1,3 +1,41 @@
 //
 
 
+const timmy = {
+    prescription: "acetaminophen",
+    pricePerRefill: 25,
+    refills: 3,
+    subscription: false,
+    coupon: true,
+};
+
+const sarah = {
+    prescription: "diphenhydramine",
+    pricePerRefill: 50,
+    refills: 1,
+    subscription: true,
+    coupon: false,
+}
+
+const rocky = {
+    prescription: "phenylephrine",
+    pricePerRefill: 30,
+    refills: 5,
+    subscription: true,
+    coupon: true,
+}
+
+
+
+function calculateTotal(customer) {
+    let refillTotal = customer.pricePerRefill * customer.refills;
+    let subscriptionDiscount = customer.subscription ? refillTotal * 0.25 : 0;
+    let couponDiscount = customer.coupon ? 10 : 0;
+    let finalAmount = refillTotal - subscriptionDiscount - couponDiscount;
+    return finalAmount;
+  }
+
+  
+console.log(`Timmy's grand total is $${calculateTotal(timmy)}.`);
+console.log(`Sarah's grand total is $${calculateTotal(sarah)}.`);
+console.log(`Rocky's grand total is $${calculateTotal(rocky)}.`);
